68bb25c28060d00b272d4ede972ed92217520501
sumeyyesert <sumeyeesert@gmail.com>
2025-09-18 02:01:51 +0300

    Initial Commit

---
+++ MovieStoreApp/MovieStoreAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieStoreAppApp.swift
+//  MovieStoreApp
+//
+//  Created by Sümeyye Sert on 18.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct MovieStoreAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
